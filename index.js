dbfe702484416604c6be4076ef2195a2030dd310
Matias Saade <matiassaade@gmail.com>
2024-04-09 11:53:17 -0300

    cors....

--- index.js
+++ index.js
@@ -11,16 +11,16 @@ const app = express()
 app.use(cors())
 app.use(express.urlencoded({extended: true, limit:'50mb'}))
 app.use(express.json())
-app.use((_req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Credentials', 'true')
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept',
-    )
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
-    next()
-})
+// app.use((_req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*')
+//     res.header('Access-Control-Allow-Credentials', 'true')
+//     res.header(
+//         'Access-Control-Allow-Headers',
+//         'Origin, X-Requested-With, Content-Type, Accept',
+//     )
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+//     next()
+// })
 app.use('/api', router);
 
 const initApp = () => {
