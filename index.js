a77951f47202cb88ccb55410df6569bc1afc05ee
Matias Saade <matiassaade@gmail.com>
2024-04-09 14:25:32 -0300

    fix: probando deferente configuracion de cors para render

--- index.js
+++ index.js
@@ -8,17 +8,37 @@ const PORT = process.env.PORT ? process.env.PORT : 3001
 
 const app = express()
 //Middlewares
-app.use(cors())
+// app.use(cors())
 app.use(express.urlencoded({extended: true, limit:'50mb'}))
 app.use(express.json())
+app.use((req, res, next) => {
+    res.setHeader(
+      "Access-Control-Allow-Origin",
+      "*"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader("Access-Control-Allow-Private-Network", true);
+    //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+    res.setHeader("Access-Control-Max-Age", 7200);
+  
+    next();
+  });
 // app.use((_req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*')
-//     res.setHeader('Access-Control-Allow-Credentials', true)
-//     res.setHeader(
+//     res.header('Access-Control-Allow-Origin', '*')
+//     res.header('Access-Control-Allow-Credentials', true)
+//     res.header(
 //         'Access-Control-Allow-Headers',
 //         'Origin, X-Requested-With, Content-Type, Accept',
 //     )
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
 //     next()
 // })
 app.use('/api', router);
