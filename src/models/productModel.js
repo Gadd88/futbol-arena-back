80428a4591c3168c5b72d82141b57d924ca31d0a
JoacooReyes <joacooreyes@gmail.com>
2024-04-18 12:51:07 -0300

    edit de stock

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -1,38 +1,34 @@
 import mongoose from "mongoose";
-import { Schema } from 'mongoose';
+import { Schema } from "mongoose";
 
 const productSchema = new Schema({
-    producto_id: {
-        unique: true,
-        type: String,
-        required: true,
-    },
-    producto: {
-        type: String,
-        required: true,
-    },
-    detalle: {
-        type: String,
-        required: true,
-    },
-    precio: {
-        type: Number,
-        required: true,
-    },
-    stock: {
-        type: Number,
-        required: true,
-    },
-    imagen: {
-        type: String,
-        required: true,
-    },
-    categoria: {
-        type: String,
-        required: true,
-    }
-})
+  producto_id: {
+    unique: true,
+    type: String,
+    required: true,
+  },
+  producto: {
+    type: String,
+    required: true,
+  },
+  detalle: {
+    type: String,
+    required: true,
+  },
+  precio: {
+    type: Number,
+    required: true,
+  },
+  imagen: {
+    type: String,
+    required: true,
+  },
+  categoria: {
+    type: String,
+    required: true,
+  },
+});
 
-const ProductModel = mongoose.model('products', productSchema, 'products')
+const ProductModel = mongoose.model("products", productSchema, "products");
 
-export default ProductModel
+export default ProductModel;
